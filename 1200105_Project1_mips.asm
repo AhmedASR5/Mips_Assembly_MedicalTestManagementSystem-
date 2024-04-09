53ed8161c0ccf6fce606a4601ab40c33bbb8bd70
AhmadASR5 <ahmadragb@hotmail.com>
2024-04-10 00:30:11 +0300

    before adding file

--- 1200105_Project1_mips.asm
+++ 1200105_Project1_mips.asm
@@ -1,5 +1,9 @@
 .data
 
+filename: .asciiz "testResults.txt"
+openFlags: .word 0x0001       # Flag for write and create
+mode: .word 0x01B6            # Mode for file permissions (0644)
+
 promptID: .asciiz "Enter Patient ID (7 digits): "
 idBuffer: .space 11 # Buffer for ID, assuming max 10 chars + null terminator
 
@@ -18,13 +22,89 @@ msgValid: .asciiz "\nValid ID.\n"
 msgInvalid: .asciiz "\nInvalid ID. Must be exactly 7 digits.\n"
 
 
+menu: .asciiz "\n--- Medical Test System Menu ---\n1. Add a new medical test\n2. Search for a test by patient ID\n3. Retrieve all up normal patient tests\n4. Retrieve all patient tests in a given specific period\n5. Search for unnormal tests\n6. Average test value\n7. Update an existing test result\n8. Delete a test\n9. Exit\nSelect an option: "
+invalidOption: .asciiz "Invalid option. Please try again.\n"
+prompt: .asciiz "Your choice: "
+
+
 .text
 .globl main
 
 main:
 
+#----------------------------------------Opening the file for writing----------------------------------------
 
-#-----------------------------------------Getting information from the user----------------------------
+# Open the file for writing
+    li $v0, 13                 # Syscall: open file
+    la $a0, filename           # Filename: pointer to "testResults.txt"
+    li $a1, openFlags          # Flags: write and create
+    li $a2, mode               # Mode: file permissions
+    syscall
+    move $s7, $v0              # Save the file descriptor to $s7
+    j menu_loop   
+
+#---------------------------------------- End of Opening the file for writing--------------------------------
+
+   
+
+#----------------------------------------Menu--------------------------------     
+
+menu_loop:
+
+    # Display menu
+    li $v0, 4
+    la $a0, menu
+    syscall
+
+    # Read user's choice
+    li $v0, 5
+    syscall
+    move $t0, $v0  # Move user's choice into $t0
+
+    # Compare user's choice and jump to the corresponding procedure
+    li $t1, 1
+    beq $t0, $t1, add_test
+    
+    li $t1, 2
+    beq $t0, $t1, search_test
+
+    li $t1, 3
+    beq $t0, $t1, retrieve_all_up_normal_tests
+    
+    li $t1, 4
+    beq $t0, $t1, retrieve_all_tests_in_period
+
+    li $t1, 5
+    beq $t0, $t1, search_unnormal_tests
+
+    li $t1, 6
+    beq $t0, $t1, average_test_value
+
+    li $t1, 7
+    beq $t0, $t1, update_existing_test_result
+
+    li $t1, 8
+    beq $t0, $t1, delete_test
+
+   # Process user's choice
+    li $t1, 9  # Exit option number
+    beq $t0, $t1, exit_program  # If user chooses to exit
+
+    # If invalid option, show error and go back to menu
+    li $v0, 4
+    la $a0, invalidOption
+    syscall
+    j menu_loop
+
+
+exit_program:
+    # Exit the program
+    li $v0, 10
+    syscall
+
+#----------------------------------------Getting information from the user (Add test) --------------------------------
+
+add_test:
     # Prompt for Patient ID
     li $v0, 4
     la $a0, promptID
@@ -87,10 +167,67 @@ main:
     sdc1 $f0, 0($t0)  # Store double-precision value from $f0-$f1 into buffer
 
 
+j menu_loop
+
+
+#------------------------------------Getting information from the user----------------------------  
+
+search_test: 
+
+    # Prompt for Patient ID
+    li $v0, 4
+    la $a0, promptID
+    syscall
+
+    # Read Patient ID as a string
+    li $v0, 8
+    la $a0, idBuffer
+    li $a1, 11 # Max length to read
+    syscall
+    
+    
+      # Validate Patient ID
+    la $a0, idBuffer # Load address of the ID buffer
+    jal validatePatientID # Jump to the validation function
+
+    # Check if ID is valid (result returned in $v0, 1 = valid, 0 = invalid)
+    li $t1, 1
+    beq $v0, $t1, validID
+    j invalidID
 
 
 
-#------------------------------------Getting information from the user----------------------------       
+
+
+retrieve_all_up_normal_tests:
+    j menu_loop
+
+
+retrieve_all_tests_in_period:
+    j  menu_loop
+
+
+search_unnormal_tests:
+    j menu_loop
+
+
+average_test_value:
+    j menu_loop
+
+
+update_existing_test_result:
+    j menu_loop
+
+
+delete_test:
+    j menu_loop
+
+
+
+
+
+
+
     
 #---------------------------------------Functions area--------------------------------------------
 validID:
